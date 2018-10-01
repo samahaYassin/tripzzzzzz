14a9f899916f3488e0a769b0cf1d4a3bb11dd77e
Yassin Samaha <yasssin_samaha07@hotmail.com>
2018-10-01 21:14:03 +0200

    a

---
+++ app/src/main/java/com/example/yasss/tripzzzzz/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.yasss.tripzzzzz;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import info.androidhive.loginandregistration.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
